41dfd392a45215cc9b8202c6718285fc8fdf5d15
Shaix33 <kevinndlondlo@gmail.com>
2025-09-04 15:10:01 +0200

    integrating frontend logic

--- binary-blossom-v1/farmweather/urls.py
+++ binary-blossom-v1/farmweather/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from farmweather.farm import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('farmweather.farm.urls')), # Frontend views
+    path('', views.home, name='home'), # Frontend views
+    path('api/', include('farmweather.farm.urls')),
 ]
